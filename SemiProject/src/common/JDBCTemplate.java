b981e3fee41f7b1b7c00a52e9670a22d740ad77e
USER <USER@LAPTOP-K6E3LRGH>
2018-12-12 10:22:57 +0900

    디비 접속 주소 변경

--- SemiProject/src/common/JDBCTemplate.java
+++ SemiProject/src/common/JDBCTemplate.java
@@ -19,7 +19,7 @@ public class JDBCTemplate {
 			e.printStackTrace();
 		}
 		
-		String url="jdbc:oracle:thin:@localhost:1521:xe";
+		String url="jdbc:oracle:thin:@192.168.10.4:1521:xe";
 		String id="test";
 		String pw="test";
 		
